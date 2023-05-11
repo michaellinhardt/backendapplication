2aad314646942093f0bb6ee491e191efad8f4695
Michael Linhardt <linhardt.michael@gmail.com>
2023-05-11 22:05:56 +0700

    initial

---
+++ string.helper.js
@@ -0,0 +1,6 @@
+export const
+
+	// Returns a new string with the first character converted to lower case.
+	lowerFirstChar = string => string.charAt(0).toLowerCase() + string.slice(1),
+	// Returns a new string with the first character converted to upper case.
+	upperFirstChar = string => string.charAt(0).toUpperCase() + string.slice(1)
